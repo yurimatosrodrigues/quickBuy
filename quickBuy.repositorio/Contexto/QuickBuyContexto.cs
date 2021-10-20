1575cf109549c192ea3b0f8a5d33b415aa74d239
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-10-20 20:35:34 -0300

    Adicionando pacotes do EF e configurando acesso ao bd

---
+++ quickBuy.repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quickBuy.dominio.Entidades;
+using quickBuy.dominio.ObjetoDeValor;
+
+namespace quickBuy.repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios{ get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedido{ get; set; }
+        public DbSet<FormaPagamento> FormaPagamento{ get; set; }
+
+        public QuickBuyContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
--- quickBuy.web/Startup.cs
+++ quickBuy.web/Startup.cs
@@ -2,24 +2,32 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using quickBuy.repositorio.Contexto;
 
 namespace quickBuy.web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+        }        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<QuickBuyContexto>(option => 
+                                                        option.UseMySql(connectionString,
+                                                                        mbox => mbox.MigrationsAssembly("QuickBuy.repositorio")));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
