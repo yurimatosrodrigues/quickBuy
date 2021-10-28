b25887a4c7f7f3569c0f7ec75b930149a34ee3f3
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-10-28 00:26:33 -0300

    implementacao do post para produtos na API

--- quickBuy.repositorio/Repositorios/PedidoRepositorio.cs
+++ quickBuy.repositorio/Repositorios/PedidoRepositorio.cs
@@ -1,9 +1,13 @@
 ﻿using quickBuy.dominio.Contratos;
 using quickBuy.dominio.Entidades;
+using quickBuy.repositorio.Contexto;
 
 namespace quickBuy.repositorio.Repositorios
 {
     public class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
     {
+        public PedidoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
+        }
     }
 }
