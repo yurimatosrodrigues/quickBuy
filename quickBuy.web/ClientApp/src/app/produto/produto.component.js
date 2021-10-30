db97df1359ca9e1feb0a0bbe6415e996d2562e78
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-10-30 02:07:23 -0300

    Atualizando node_modules

---
+++ quickBuy.web/ClientApp/src/app/produto/produto.component.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProdutoComponent = void 0;
+var ProdutoComponent = /** @class */ (function () {
+    function ProdutoComponent() {
+    }
+    //camelCase para variáveis, atributos e funções.
+    ProdutoComponent.prototype.obterNome = function () {
+        return this.nome;
+    };
+    return ProdutoComponent;
+}());
+exports.ProdutoComponent = ProdutoComponent;
+//# sourceMappingURL=produto.component.js.map
---
+++ quickBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "produto", //nome da tag HTML onde o componente vai ser renderizado
+    template: "" // estrutura do HTML 
+})
+export class ProdutoComponent {    //export disponibiliza a classe para utilização no módulo. Torna "public"
+    public nome: string = 'teste';
+    public liberadoParaVenda: boolean;
+
+    //camelCase para variáveis, atributos e funções.
+    public obterNome(): string {
+        return this.nome;
+    }
+
+}
