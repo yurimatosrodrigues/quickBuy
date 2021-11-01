aa86056fba9e07613ae8d315382d21a80b0bb54c
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-11-01 20:19:13 -0300

    Criação da tela Login

--- quickBuy.web/ClientApp/src/app/app.module.ts
+++ quickBuy.web/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { LoginComponent } from './usuario/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +19,19 @@ import { ProdutoComponent } from './produto/produto.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent },
-    { path: 'produto', component: ProdutoComponent }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'produto', component: ProdutoComponent },
+      {path: 'entrar', component: LoginComponent}
 ], { relativeLinkResolution: 'legacy' })
   ],
   providers: [],
--- quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,10 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+
+}
