30b4453a6296bb5dec4e29907c3dfc08779f1f34
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-10-31 21:31:46 -0300

    Adicionando componente login

--- quickBuy.web/ClientApp/src/app/produto/produto.component.ts
+++ quickBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core"
 
 @Component({
     selector: "app-produto", //nome da tag HTML onde o componente vai ser renderizado
-    template: "" // estrutura do HTML 
+    template: "<html><body>{{ obterNome() }}</body></html>" // estrutura do HTML 
 })
 export class ProdutoComponent {    //export disponibiliza a classe para utilização no módulo. Torna "public"
     public nome: string = 'teste';
