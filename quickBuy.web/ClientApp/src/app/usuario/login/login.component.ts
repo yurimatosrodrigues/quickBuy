3f9047cc2724aea20b1dc7884862d7fea7cc1936
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-11-01 21:01:01 -0300

    Adicionando imagem e entendendo BINDING

--- quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-
+  public email = "yuimatos.tuti@gmail.com";
+  
+  entrar() {
+    alert(this.email);
+  }
+  
 }
