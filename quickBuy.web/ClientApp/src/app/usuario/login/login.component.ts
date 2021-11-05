77e9321bae7f393c0b535fe734f69962b2718998
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-11-05 00:47:00 -0300

    Implementando o OnInit no componente de login

--- quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ quickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,24 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   public usuario;
-  //private router: Router;
+  public returnUrl: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {    
+  }
+
+  ngOnInit(): void{
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
-  entrar() {    
+  entrar() {
     if (this.usuario.email == "teste@teste.com.br" && this.usuario.senha == "12345678") {
       sessionStorage.setItem("usuario-autenticado", "1"); //acontece dentro do contexto do usuário
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUrl]);
     }
   }  
 }
