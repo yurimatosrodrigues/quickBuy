9c027cb8e24a291a2f25690f80eceb547b741927
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-12-29 21:24:12 -0300

    Injetando UsuarioServico no guarda-rotas e nav-menu

--- quickBuy.web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ quickBuy.web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsuarioServico } from '../servicos/usuario/usuario.servico';
 
 @Component({
   selector: 'app-nav-menu',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 export class NavMenuComponent {
   isExpanded = false;
 
-  constructor(private router: Router){
+  constructor(private router: Router, private usuarioServico: UsuarioServico){
 
   }
 
